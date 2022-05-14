352cfb10674c5c19fa80ca175a1d6d78b350b5e1
Leeharin115 <lovehaha11@gmail.com>
2022-05-14 18:38:39 +0900

    add youtube service

--- src/App.jsx
+++ src/App.jsx
@@ -4,44 +4,19 @@ import VideoSearchHeader from './components/VIDEO/VideoSearchHeader/VideoSearchH
 import VideoList from './components/VIDEO/VideoList/VideoList';
 import styles from './App.module.css';
 
-const App = () => {
-  const apiKey = process.env.REACT_APP_API_KEY;
+const App = ({ youtube }) => {
   const [videos, setVideos] = useState([]);
-  const [keyword, setKeyword] = useState('');
 
   useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    };
-    fetch(
-      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&regionCode=KR&maxResults=26&key=${apiKey}`,
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        setVideos(data.items);
-      })
-      .catch((error) => console.log('error', error));
+    youtube
+      .mostPopular() //
+      .then((items) => setVideos(items));
   }, []);
 
   const searchHandler = (keyword) => {
-    setKeyword(keyword);
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    };
-    fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=26&q=${keyword}&regionCode=KR&type=video&key=${apiKey}`,
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => {
-        setVideos(
-          result.items.map((item) => ({ ...item, id: item.id.videoId }))
-        );
-      })
-      .catch((error) => console.log('error', error));
+    youtube
+      .search(keyword) //
+      .then((items) => setVideos(items));
   };
   return (
     <>
--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Youtube from './service/youtube';
+
+const apiKey = process.env.REACT_APP_API_KEY;
+const youtube = new Youtube(apiKey);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App youtube={youtube} />
   </React.StrictMode>
 );
